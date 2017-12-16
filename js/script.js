a719a33f4a9d77de649e40ccd76c02d389ca6818
huangchengtx <930077684@qq.com>
2017-12-16 09:14:24 +0800

    flexible

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
+(function(win) {
+    var doc = win.document;
+    var docEl = doc.documentElement;
+    var tid;
+
+    function refreshRem() {
+        var width = docEl.getBoundingClientRect().width;
+        if (width > 414) { // 最大宽度
+            width = 414;
+        }
+        var rem = width / 7.5;
+        docEl.style.fontSize = rem + 'px';
+    }
+
+    win.addEventListener('resize', function() {
+        clearTimeout(tid);
+        tid = setTimeout(refreshRem, 0);
+    }, false);
+    win.addEventListener('pageshow', function(e) {
+        if (e.persisted) {
+            clearTimeout(tid);
+            tid = setTimeout(refreshRem, 0);
+        }
+    }, false);
+
+    refreshRem();
+
+})(window);
+
 var swiperV = new Swiper('.swiper-container-v', {
     direction: 'vertical',
     mousewheel: true,
